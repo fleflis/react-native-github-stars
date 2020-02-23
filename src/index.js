d93e0e875c19dea49a682e4aa4ac0198e60bc515
Christian Sanches <christian@endless-point.org>
2020-02-23 18:55:19 -0300

    Reactotron

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {StatusBar, View} from 'react-native';
+import './config/reactotronConfig';
+
+const App = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <View />
+        </>
+    );
+};
+
+export default App;
